6fedf84f2ca7e1c4388c44f17763c7046426de48
Esolitos <esolitos@gmail.com>
2018-02-19 16:20:59 +0100

    Cleanup and complete mysql backups script.

--- shell/_utils.sh
+++ shell/_utils.sh
@@ -4,20 +4,44 @@
 #
 
 #
+# Prints a log message only if VERBOSE or DEBUG are defined
+#
+verbose_printer() {
+  if [ -z "$VERBOSE" -a -z "$DEBUG" ]; then
+    return 0
+  fi;
+  printf "%s\n" "$1"
+}
+
+#
+# Prints a message into the stderr, in red if supported
+#
+error_printer() {
+  string-format-get
+  printf "${FMT_red}%s${FMT_normal}\n" "$1" 1>&2
+}
+
+#
 # Includes the standard config files in the logical order
 #
-include-conf-file() {
+include_conf_file() {
   # Only one parameter should be passed, the config file name
   if [ $# -ne 1 ]; then
+    error_printer "One argument is required: config filename"
     return 1
   fi
+
   # Global config file
-  if [ -r "/etc/$CONF_FILE" ]; then
-    source "/etc/$CONF_FILE"
+  cfg_global="/etc/$1"
+  if [ -f "$cfg_global" -a -r "$cfg_global" ]; then
+    verbose_printer "Sourcing global conf: $cfg_global"
+    . "$cfg_global"
   fi
   # Current user config file
-  if [ -r "$HOME/.config/$CONF_FILE" ]; then
-    source "$HOME/.config/$CONF_FILE"
+  cfg_user="$HOME/.config/$1"
+  if [ -f "$cfg_user" -a -r "$cfg_user" ]; then
+    verbose_printer "Sourcing user conf: $cfg_user"
+    . "$cfg_user"
   fi
 }
 
@@ -25,7 +49,7 @@ include-conf-file() {
 # Checks if terminal supports color and if so defines a few FMT_* constants to
 #  allow wasy color swapping.
 #
-string-format-get() {
+string_format_get() {
   # check if stdout is a terminal...
   if test -t 1; then
     # see if it supports colors...
@@ -49,19 +73,20 @@ string-format-get() {
 }
 
 #
-# Prints a log message only if VERBOSE or DEBUG are defined
+# Checks if BackBlaze command (b2) is available and configured
 #
-verbose-only-printer() {
-  if [[ -z "$VERBOSE" -a -z "$DEBUG" ]]; then
-    return 0
-  fi;
-  printf "%s\n" $1
-}
+check_backblaze_status() {
+  which b2 >/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    error_printer "Missing b2 executable in path."
+    return 1
+  fi
 
-#
-# Prints a message into the stderr, in red if supported
-#
-error-printer() {
-  string-format-get
-  printf "${FMT_red}%s${FMT_normal}\n" $1 1>&2
+  b2 get-account-info >/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    error_printer "b2 account not configured"
+    return 2
+  fi
+
+  return 0
 }
--- shell/mysql-backups.sh
+++ shell/mysql-backups.sh
@@ -6,38 +6,54 @@
 # - Cleanup old backups
 #
 
+# Enable debug mode
+if [ ! -z "$DEBUG" ]; then
+  set -x
+  VERBOSE=1
+fi
+
+UTILS_FILE=$(realpath "$(dirname $0)/_utils.sh")
+if [ ! -r $UTILS_FILE  ]; then
+  echo "Missing required _utils.sh file."
+  exit 1
+else
+  . $UTILS_FILE
+fi
+
 # Include config
-if [ -f "$HOME/.eso-conf/mysql-backups" ]; then
-  source "$HOME/.eso-conf/mysql-backups"
-fi;
+CONF_FILENAME="esolitos-mysql-backups"
+include_conf_file $CONF_FILENAME
 
 # Set some default values if not provided
-
 if [ -z "$BKP_DATE" ]; then
   BKP_DATE="$(date '+%Y-%W')"
-fi;
+fi
 
 if [ -z "$BKP_DIR" ]; then
   BKP_DIR='/var/backups/mysql'
-fi;
+fi
 
 if [ -z "$MYSQL_DEFAULTS" ]; then
   MYSQL_DEFAULTS='/etc/mysql/debian.cnf'
-fi;
+fi
 
-# Create weekly db
+# Create snapshot of all mysql databases
 mkdir -p "$BKP_DIR/$BKP_DATE"
 DB_LIST=$(mysql --defaults-file="$MYSQL_DEFAULTS" --local-infile --batch --skip-column-names -e "SHOW DATABASES;" | grep -vE '(performance_schema)')
 for dbname in $DB_LIST; do
+  verbose_printer "Backing up: $dbname with destination $BKP_DIR/$BKP_DATE/$dbname.sql.gz"
   mysqldump --defaults-file="$MYSQL_DEFAULTS" --add-drop-table --add-locks --comments --flush-privileges --lock-all-tables --dump-date "$dbname" | pigz -c > "$BKP_DIR/$BKP_DATE/$dbname.sql.gz"
-done;
+done
 
 # Upload to BackBlaze
-which b2 >/dev/null 2>&1
-if [ $? -ne 0 -o -z "$B2_BUCKET" ]; then
-  printf "Backups not uploaded!" 1>&2
+if [ ! check_backblaze_status ]; then
+  error_printer "Mysql backups not mirrored: missing b2 command"
   exit 10
+elif [ -z "$B2_BUCKET" ]; then
+  error_printer "Mysql backups not mirrored: missing bucket name"
+  exit 11
 else
   # Sync weekly directory with remote one.
-  b2 sync --keepDays 120 --noProgress "$BKP_DIR/$BKP_DATE" "b2://${B2_BUCKET}/mysql/"
-fi;
+  verbose_printer "Mirroring backups to b2://$B2_BUCKET/$B2_SUB_PATH"
+  verbose_printer "$(b2 sync --keepDays 120 --noProgress "$BKP_DIR/$BKP_DATE" "b2://$B2_BUCKET/$B2_SUB_PATH")"
+fi
